50d2c5eb44155dfa687188daa8a8d24be7e82fb6
olefredrik <mail@olefredrik.com>
2013-12-05 22:58:38 +0100

    added off-canvas menu

--- footer.php
+++ footer.php
@@ -5,6 +5,11 @@
 	<?php dynamic_sidebar("Footer"); ?>
 </footer>
 
+<a class="exit-off-canvas"></a>
+
+ </div>
+</div>
+
 <?php wp_footer(); ?>
 
 <script>
--- header.php
+++ header.php
@@ -12,14 +12,42 @@
 
   </head>
   <body <?php body_class(); ?>>
+
+  <div class="off-canvas-wrap">
+  <div class="inner-wrap">
+
+  <nav class="tab-bar show-for-small-only">
+    <section class="left-small">
+      <a class="left-off-canvas-toggle menu-icon" ><span></span></a>
+    </section>
+    <section class="middle tab-bar-section">
+      <h1 class="title"><?php bloginfo( 'name' ); ?></h1>
+    </section>
+  </nav>
+
+  <aside class="left-off-canvas-menu">
+      <?php
+          wp_nav_menu( array(
+              'theme_location' => 'primary',
+              'container' => false,
+              'depth' => 0,
+              'items_wrap' => '<ul class="off-canvas-list">%3$s</ul>',
+              'fallback_cb' => 'FoundationPress_menu_fallback', // workaround to show a message to set up a menu
+              'walker' => new FoundationPress_walker( array(
+                  'in_top_bar' => true,
+                  'item_type' => 'li',
+                  'menu_type' => 'main-menu'
+              ) ),
+          ) );
+      ?>
+  </aside>
   
   <div class="contain-to-grid">
-    <nav class="top-bar" data-topbar>
+    <nav class="top-bar show-for-medium-up" data-topbar>
       <ul class="title-area">
         <li class="name">
           <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
         </li>
-        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
       </ul>
    
       <section class="top-bar-section">
