e9d381940e906ee16f383ca4eea0dfef61b9d045
olefredrik <mail@olefredrik.com>
2013-12-08 19:49:48 +0100

    run jquery in noconflict mode

--- footer.php
+++ footer.php
@@ -12,11 +12,5 @@
 
 <?php wp_footer(); ?>
 
-<script>
-	(function($) {
-		$(document).foundation();
-	})(jQuery);
-</script>
-	
 </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,1 +1,2 @@
-$(document).foundation();
+jQuery.noConflict();
+jQuery(document).foundation();
