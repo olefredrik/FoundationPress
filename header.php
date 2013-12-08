21732472c43671a862a1b08bf8a418ebdcd6b636
olefredrik <mail@olefredrik.com>
2013-12-08 20:01:43 +0100

    added data-topbar attribute to nav element. submenus now work as expected

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
   </aside>
   
         <div class="top-bar-container contain-to-grid show-for-medium-up">
-            <nav class="top-bar">
+            <nav class="top-bar" data-topbar="">
                 <ul class="title-area">
                     <li class="name">
                         <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
