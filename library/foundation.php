30f183c53b94b0ab3f9a8005ae0f523a382e8d34
Dawid Loranc <dawidloranc@gmail.com>
2014-02-03 16:48:15 +0100

    Fixed href attribute.

--- library/foundation.php
+++ library/foundation.php
@@ -19,7 +19,7 @@ function FoundationPress_pagination() {
 	) );
  
 	$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination'>", $paginate_links );
-	$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'><a href=''>", $paginate_links );
+	$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'><a href='#'>", $paginate_links );
 	$paginate_links = str_replace( "</span>", "</a>", $paginate_links );
 	$paginate_links = preg_replace( "/\s*page-numbers/", "", $paginate_links );
 
