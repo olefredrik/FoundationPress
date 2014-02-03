d6096ec9de0ce0c32e5956eb4635797c4b352c9b
Dawid Loranc <dawidloranc@gmail.com>
2014-02-03 16:00:35 +0100

    Fixed pagination.

--- library/foundation.php
+++ library/foundation.php
@@ -18,6 +18,11 @@ function FoundationPress_pagination() {
 		'type' => 'list'
 	) );
  
+	$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination'>", $paginate_links );
+	$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'><a href=''>", $paginate_links );
+	$paginate_links = str_replace( "</span>", "</a>", $paginate_links );
+	$paginate_links = preg_replace( "/\s*page-numbers/", "", $paginate_links );
+
 	// Display the pagination if more than one page is found
 	if ( $paginate_links ) {
 		echo '<div class="pagination-centered">';
