494ee2e36ec18ee228994d65246cfa6510fdf25f
olefredrik <mail@olefredrik.com>
2014-03-08 21:30:29 +0100

    minor updates

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html class="no-js" <?php language_attributes(); ?> >
   <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php if ( is_category() ) {
       echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		
 	<?php endif;?>
 	
-	<?php if ( function_exists('FoundationPress_pagination') ) { foundationPress_pagination(); } else if ( is_paged() ) { ?>
+	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
 		<nav id="post-nav">
 			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
 			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
