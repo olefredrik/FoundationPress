3b2e70a0b3f94c97b4523a263b677e5a00a8c389
olefredrik <mail@olefredrik.com>
2014-03-08 21:14:30 +0100

    added a more seo friendly title tag. added google chrome frame for ie

--- header.php
+++ header.php
@@ -2,8 +2,26 @@
 <html class="no-js" <?php language_attributes(); ?> >
   <head>
     <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+    <title><?php if ( is_category() ) {
+      echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
+    } elseif ( is_tag() ) {
+      echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
+    } elseif ( is_archive() ) {
+      wp_title(''); echo ' Archive | '; bloginfo( 'name' );
+    } elseif ( is_search() ) {
+      echo 'Search for &quot;'.wp_specialchars($s).'&quot; | '; bloginfo( 'name' );
+    } elseif ( is_home() || is_front_page() ) {
+      bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
+    }  elseif ( is_404() ) {
+      echo 'Error 404 Not Found | '; bloginfo( 'name' );
+    } elseif ( is_single() ) {
+      wp_title('');
+    } else {
+      echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
+    } ?></title>
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css" />
     <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
     <?php wp_head(); ?>
