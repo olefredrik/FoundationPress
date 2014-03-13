4059738d5a7f4e32339ebb539ff734a8fa5cd86e
Ole Fredrik Lie <mail@olefredrik.com>
2014-03-13 15:45:22 +0100

    Remove default inline style of wp-caption

--- library/cleanup.php
+++ library/cleanup.php
@@ -119,6 +119,34 @@ function gallery_style($css) {
  * ----------------------------------------------------------------------------
  */
 
+
+// Remove default inline style of wp-caption
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+    if ( ! isset( $attr['caption'] ) ) {
+        if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+            $content = $matches[1];
+            $attr['caption'] = trim( $matches[2] );
+        }
+    }
+    $output = apply_filters('img_caption_shortcode', '', $attr, $content);
+    if ( $output != '' )
+        return $output;
+    extract(shortcode_atts(array(
+        'id'    => '',
+        'align' => 'alignnone',
+        'width' => '',
+        'caption' => ''
+    ), $attr));
+    if ( 1 > (int) $width || empty($caption) )
+        return $content;
+    if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+    return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: ' . $width . 'px">'
+    . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
+
+
 // Clean the output of attributes of images in editor
 function image_tag_class($class, $id, $align, $size) {
     $align = 'align' . esc_attr($align);
