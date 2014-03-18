d00086ac2956aab5c77e8db0906a1dbd4190aae2
Ole Fredrik Lie <mail@olefredrik.com>
2014-03-18 23:45:32 +0100

    corrected jquery path in enqueue scripts. added foundation init.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -8,7 +8,7 @@ if (!function_exists('FoundationPress_scripts')) :
 
     // enqueue modernizr, jquery and foundation
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery/jquery.min.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', true );
     wp_enqueue_script( 'foundation', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true );
 
   }
