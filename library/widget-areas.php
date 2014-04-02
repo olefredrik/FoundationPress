6272d50b9bbb5d629369ab2dd6434931ffbacf14
NejcDraganjec <nejc.draganjec@gmail.com>
2014-04-02 17:34:42 +0200

    Add hooks to FoundationPress widgets registration
    Add hooks to FoundationPress widget registration so that it is easy to unregister them from child theme function.php.

--- library/widget-areas.php
+++ library/widget-areas.php
@@ -1,20 +1,33 @@
 <?php
-$sidebars = array('Sidebar');
-foreach ($sidebars as $sidebar) {
-    register_sidebar(array('name'=> $sidebar,
-        'before_widget' => '<article id="%1$s" class="row widget %2$s"><div class="small-12 columns">',
-        'after_widget' => '</div></article>',
-        'before_title' => '<h6><strong>',
-        'after_title' => '</strong></h6>'
-    ));
+function foundationpress_sidebars_widgets() {
+    $sidebars = array('Sidebar');
+    foreach ($sidebars as $sidebar) {
+        register_sidebar(array(
+            'name'=> $sidebar,
+            'id'=> 'foundationpress_sidebar',
+            'before_widget' => '<article id="%1$s" class="row widget %2$s"><div class="small-12 columns">',
+            'after_widget' => '</div></article>',
+            'before_title' => '<h6><strong>',
+            'after_title' => '</strong></h6>'
+        ));
+    }
+    do_action('framework_sidebars');
 }
-$sidebars = array('Footer');
-foreach ($sidebars as $sidebar) {
-    register_sidebar(array('name'=> $sidebar,
-        'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
-        'after_widget' => '</article>',
-        'before_title' => '<h6><strong>',
-        'after_title' => '</strong></h6>'
-    ));
+add_action( 'widgets_init', 'foundationpress_sidebars_widgets' );
+
+function foundationpress_footer_widgets() {
+    $sidebars = array('Footer');
+    foreach ($sidebars as $sidebar) {
+        register_sidebar(array(
+            'name'=> $sidebar,
+            'id'=> 'foundationpress_footer',
+            'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
+            'after_widget' => '</article>',
+            'before_title' => '<h6><strong>',
+            'after_title' => '</strong></h6>'
+        ));
+    }
+    do_action('framework_footerwidget');
 }
+add_action( 'widgets_init', 'foundationpress_footer_widgets' );
 ?>
