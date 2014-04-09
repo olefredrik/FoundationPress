00aa377146ddf9bde0a61690590d886bfff34e9e
olefredrik <mail@olefredrik.com>
2014-04-09 20:52:20 +0200

    All scripts should now be registered and enqueued properly. jQuery will be referenced in the header, in order to avoid compability issues with third party plugins.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -6,10 +6,15 @@ if (!function_exists('FoundationPress_scripts')) :
     // deregister the jquery version bundled with wordpress
     wp_deregister_script( 'jquery' );
 
-    // enqueue modernizr, jquery and foundation
-    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', true );
-    wp_enqueue_script( 'foundation', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true );
+    // register scripts
+    wp_register_script( 'modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
+    wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', false );
+    wp_register_script( 'foundation', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true );
+
+    // enqueue scripts
+    wp_enqueue_script('modernizr');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('foundation');
 
   }
 
