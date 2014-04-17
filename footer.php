6a6a7237c5c89af6b5773ec0ca5c9517100db26f
PixelbarEupen <info@pixelbar.be>
2014-04-17 12:22:07 +0200

    first commit with do_action hooks

--- footer.php
+++ footer.php
@@ -1,11 +1,17 @@
 	</div>
 </section>
 
-<footer class="row"><?php dynamic_sidebar("footer-widgets"); ?></footer>
+<footer class="row">
+	<?php do_action('foundationPress_before_footer'); ?>
+	<?php dynamic_sidebar("footer-widgets"); ?>
+	<?php do_action('foundationPress_after_footer'); ?>
+</footer>
 <a class="exit-off-canvas"></a>
-
+	
+  <?php do_action('foundationPress_layout_end'); ?>
   </div>
 </div>
 <?php wp_footer(); ?>
+<?php do_action('foundationPress_before_closing_body'); ?>
 </body>
 </html>
