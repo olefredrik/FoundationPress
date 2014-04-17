65f41c565611d0f1a8372261f5b3abc5bcb7ebf8
PixelbarEupen <info@pixelbar.be>
2014-04-17 12:22:23 +0200

    first commit with do_action hooks

--- search.php
+++ search.php
@@ -2,6 +2,8 @@
 
 	<div class="small-12 large-8 columns" role="main">
 	
+		<?php do_action('foundationPress_before_content'); ?>
+	
 		<h2><?php _e('Search Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h2>
 	
 	<?php if ( have_posts() ) : ?>
@@ -15,12 +17,17 @@
 		
 	<?php endif;?>
 	
+	<?php do_action('foundationPress_before_pagination'); ?>
+	
 	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+		
 		<nav id="post-nav">
 			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
 			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
 		</nav>
 	<?php } ?>
+
+	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
 	<?php get_sidebar(); ?>
