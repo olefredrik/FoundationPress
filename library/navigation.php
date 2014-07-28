d0c6e4302d38d3b83626c6ca171ac2003cf5c1b4
Ole Fredrik Lie <mail@olefredrik.com>
2014-07-28 15:09:18 +0200

    Added support for buttons in the top-bar menu

--- library/navigation.php
+++ library/navigation.php
@@ -72,4 +72,17 @@ function foundationPress_mobile_off_canvas() {
     ));
 }
 
+/** 
+ * Add support for buttons in the top-bar menu: 
+ * 1) In WordPress admin, go to Apperance -> Menus. 
+ * 2) Click 'Screen Options' from the top panel and enable 'CSS CLasses' and 'Link Relationship (XFN)'
+ * 3) On your menu item, type 'has-form' in the CSS-classes field. Type 'button' in the XFN field
+ * 4) Save Menu. Your menu item will now appear as a button in your top-menu
+*/
+
+function add_menuclass($ulclass) {
+return preg_replace('/<a rel="button"/', '<a rel="button" class="button"', $ulclass, 1);
+}
+add_filter('wp_nav_menu','add_menuclass');
+
 ?>
