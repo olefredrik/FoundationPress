13ced2d00db658c73202376abec8350a85635f85
Ole Fredrik Lie <mail@olefredrik.com>
2014-07-30 15:59:29 +0200

    Added basic woocommerce support

---
+++ woocommerce.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<div class="row">
+	<div class="small-12 large-8 columns" role="main">
+
+	<?php do_action('foundationPress_before_content'); ?>
+
+	<?php while (woocommerce_content()) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<header>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header>
+			<?php do_action('foundationPress_page_before_entry_content'); ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+			<footer>
+				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php do_action('foundationPress_page_before_comments'); ?>
+			<?php comments_template(); ?>
+			<?php do_action('foundationPress_page_after_comments'); ?>
+		</article>
+	<?php endwhile;?>
+
+	<?php do_action('foundationPress_after_content'); ?>
+
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
