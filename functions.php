212cb9ad89c04051a1fdbb9112afcb706e9b9aa9
mvillagordo <m.villagordovera@gmail.com>
2014-12-15 11:17:31 +0100

    Delete call function

--- functions.php
+++ functions.php
@@ -29,7 +29,4 @@ require_once('library/enqueue-scripts.php');
 // Add theme support
 require_once('library/theme-support.php');
 
-// Add Header image
-require_once('library/custom-header.php');
-
 ?>
