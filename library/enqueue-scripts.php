3c8fcd5ba699b42393eb74b718ecbbb1daa054d7
olefredrik <mail@olefredrik.com>
2015-02-24 21:04:01 +0100

    Add correct version numbers on registered scripts

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -7,12 +7,12 @@ if (!function_exists('FoundationPress_scripts')) :
     wp_deregister_script( 'jquery' );
 
     // register modernizr.js and put it in the <head> tag
-    wp_register_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '1.0.0', false );
+    wp_register_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false );
 
     // register jquery.js, foundation.js and put them before the </body> tag
     // if you'd like to cherry-pick the foundation components you need in your project, see Gruntfile.js lines 67-88
-    wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '1.0.0', true );
-    wp_register_script( 'foundation', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '1.0.0', true );
+    wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '2.1.3', true );
+    wp_register_script( 'foundation', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '5.5.1', true );
 
     // enqueue all registered scripts
     wp_enqueue_script('modernizr');
