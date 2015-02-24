d42208ad2170d8a2ff0d1fa67c0bedf1a874e23c
olefredrik <mail@olefredrik.com>
2015-02-24 21:48:18 +0100

    Add the CDN version of jQuery. Write better explanations to each script

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -3,19 +3,28 @@
 if (!function_exists('FoundationPress_scripts')) :
   function FoundationPress_scripts() {
 
-    // deregister the jquery version bundled with wordpress
+    // Deregister the jquery version bundled with wordpress
     wp_deregister_script( 'jquery' );
 
-    // register modernizr.js and put it in the <head> tag
+    // Modernizr is used for polyfills and feature detection. Must be placed in header. (Not required)
     wp_register_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false );
 
-    // register jquery.js, foundation.js and put them before the </body> tag
-    // if you'd like to cherry-pick the foundation components you need in your project, see Gruntfile.js lines 67-88
-    wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '2.1.3', true );
+    // Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header. (Not required)
+    wp_register_script( 'fastclick', get_template_directory_uri() . '/js/vendor/fastclick.js', array(), '1.0.0', false );
+
+    // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+    wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+
+    // Self hosted jQuery placed in the footer. (Comment the script above and uncomment the script below if you want to switch).
+    //wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '2.1.3', true );
+
+    // If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88
+    // It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
     wp_register_script( 'foundation', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '5.5.1', true );
 
-    // enqueue all registered scripts
+    // Enqueue all registered scripts
     wp_enqueue_script('modernizr');
+    wp_enqueue_script('fastclick');
     wp_enqueue_script('jquery');
     wp_enqueue_script('foundation');
 
