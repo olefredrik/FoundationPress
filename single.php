1500081cd756c17aa58f8fc7964c81e287da2f44
Josh Rathke <josh.rathke@ywammontana.org>
2015-03-28 11:20:52 -0600

    A Few Fixes For Uncaught Errors on Initial Build

--- single.php
+++ single.php
@@ -13,7 +13,7 @@
 			<?php do_action( 'foundationPress_post_before_entry_content' ); ?>
 			<div class="entry-content">
 
-			<?php if ( has_post_thumbnail() ): ?>
+			<?php if ( has_post_thumbnail() ) : ?>
 				<div class="row">
 					<div class="column">
 						<?php the_post_thumbnail( '', array('class' => 'th') ); ?>
