f687310b2de823500872ca93ded40b7bf887f3e5
Josh Rathke <josh.rathke@ywammontana.org>
2015-03-30 16:32:25 -0600

    Fixed A Few Missed Function Names
    Changed all instances of FoundationPress_entry_meta() to
    foundationpress_entry_meta().

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php FoundationPress_entry_meta(); ?>
+				<?php foundationpress_entry_meta(); ?>
 			</header>
 			<?php do_action( 'foundationPress_post_before_entry_content' ); ?>
 			<div class="entry-content">
