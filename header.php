6e0374dae07ea4f7e5290c07c6fc5fc5f12a359c
Ole Fredrik Lie <mail@olefredrik.com>
2015-04-27 23:34:36 +0200

    Fixed typo in header template

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 			echo 'Search for &quot;'.esc_html( $s ).'&quot; | '; bloginfo( 'name' );
 		} elseif ( is_home() || is_front_page() ) {
 			bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
-		}  elseif ( is_404() ) {
+		} elseif ( is_404() ) {
 			echo 'Error 404 Not Found | '; bloginfo( 'name' );
 		} elseif ( is_single() ) {
 			wp_title( '' );
