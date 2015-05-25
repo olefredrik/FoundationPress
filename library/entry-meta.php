c9440463fbd9d37c69dcfb8670d370beec3ca6a5
Ole Fredrik Lie <mail@olefredrik.com>
2015-05-25 16:40:46 +0200

    Fixed build errors in cleanup

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -6,6 +6,7 @@
  * @subpackage FoundationPress
  * @since FoundationPress 1.0
  */
+
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
 		echo '<time class="updated" datetime="'. get_the_time( 'c' ) .'">'. sprintf( __( 'Posted on %s at %s.', 'foundationpress' ), get_the_date(), get_the_time() ) .'</time>';
