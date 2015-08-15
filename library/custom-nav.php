9790c3bd2a28c5a4beaf988566ea0a772cd1e88a
Adam Chlan <achlan@gmail.com>
2015-08-15 16:33:49 -0400

    fix WP unit test failures

--- library/custom-nav.php
+++ library/custom-nav.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Allow users to select Topbar or Offcanvas menu. Adds body class of offcanvas or topbar based on which they choose. 
+ * Allow users to select Topbar or Offcanvas menu. Adds body class of offcanvas or topbar based on which they choose.
  *
  * @package WordPress
  * @subpackage FoundationPress
@@ -8,7 +8,6 @@
  */
 
 function wpt_register_theme_customizer( $wp_customize ) {
-
 	// Create custom panels
 	$wp_customize->add_panel( 'mobile_menu_settings', array(
 	  'priority' => 1000,
@@ -18,7 +17,7 @@ function wpt_register_theme_customizer( $wp_customize ) {
 	) );
 
 	// Create custom nav field
-	$wp_customize->add_section ( 'mobile_menu_layout' , array (
+	$wp_customize->add_section( 'mobile_menu_layout' , array (
 		'title'	=> __('Offcanvas or Topbar','foundationpress'),
 		'panel' => 'mobile_menu_settings',
 		'priority' => 1000,
@@ -28,16 +27,16 @@ function wpt_register_theme_customizer( $wp_customize ) {
 	$wp_customize->add_setting(
 		'wpt_mobile_menu',
 		array(
-			'default'	=> __( 'offcanvas', 'foundationpress' )
+			'default'	=> __( 'offcanvas', 'foundationpress' ),
 		)
 	);
 
 	// Add radio button options
-	$wp_customize->add_control (
+	$wp_customize->add_control(
 		new WP_Customize_Control(
 			$wp_customize,
 			'mobile_menu_layout',
-			array (
+			array(
 				'type'		=> 'radio',
 				'label'		=> __('Offcanvas or Topbar', 'foundationpress'),
 				'section' 	=> 'mobile_menu_layout',
@@ -52,10 +51,9 @@ function wpt_register_theme_customizer( $wp_customize ) {
 }
 add_action( 'customize_register', 'wpt_register_theme_customizer' );
 
-// Add class to body to help w/ CSS 
+// Add class to body to help w/ CSS
 add_filter( 'body_class', 'mobile_nav_class' );
 function mobile_nav_class( $classes ) {
-	
 	if ( get_theme_mod( 'wpt_mobile_menu' ) == 'offcanvas' ) :
 		$classes[] = 'offcanvas';
 	elseif ( get_theme_mod( 'wpt_mobile_menu' ) == 'topbar' ) :
