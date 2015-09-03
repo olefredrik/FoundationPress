034718aba86e9096b6336c7b302ffbad39ef06bf
Ole Fredrik Lie <mail@olefredrik.com>
2015-09-03 14:56:20 +0200

    Fix build errors

--- parts/featured-image.php
+++ parts/featured-image.php
@@ -1,6 +1,6 @@
 <?php
 	// If a feature image is set, get the id, so it can be injected as a css background property
-	if (has_post_thumbnail( $post->ID ) ):
+	if ( has_post_thumbnail( $post->ID ) ) :
 		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 		$image = $image[0];
 		echo '<header id="featured-hero" role="banner" style="background-image: url(\'' . $image .  '\')" > ';
