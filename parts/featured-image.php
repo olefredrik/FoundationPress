7c98bd3aee3f0c4b2304adf9a6699eb6ae072bb0
olefredrik <mail@olefredrik.com>
2015-09-19 16:44:18 +0200

    Only render the header tag if a featured image is set

--- parts/featured-image.php
+++ parts/featured-image.php
@@ -3,11 +3,6 @@
 	if ( has_post_thumbnail( $post->ID ) ) :
 		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 		$image = $image[0];
-		echo '<header id="featured-hero" role="banner" style="background-image: url(\'' . $image .  '\')" > ';
-
-	// If no feature image is set, just render the header tag without injected css background
- 	else :
-		echo '<header id="featured-hero" class="no-featured-image-set" role="banner">';
+		echo '<header id="featured-hero" role="banner" style="background-image: url(\'' . $image .  '\')" ></header>';
 	endif;
 ?>
-</header>
