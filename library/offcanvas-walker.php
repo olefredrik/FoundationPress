925510aa3b589823e687f27d2dc7ec02446b071f
olefredrik <mail@olefredrik.com>
2015-11-21 11:09:15 +0100

    Add credit for offcanvas walker

--- library/offcanvas-walker.php
+++ library/offcanvas-walker.php
@@ -7,9 +7,9 @@
  * @since FoundationPress 1.0.0
  */
 
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+
 if ( ! class_exists( 'Foundationpress_Offcanvas_Walker' ) ) :
-
-
 class Foundationpress_Offcanvas_Walker extends Walker_Nav_Menu {
 	function start_lvl(&$output, $depth = 0, $args = Array() ) {
 			$indent = str_repeat("\t", $depth);
