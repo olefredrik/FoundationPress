9e4960316354182482d6801867a7008956ad6a9b
Josh Rathke <josh.rathke@ywammontana.org>
2015-12-02 00:21:40 -0700

    Wordpress Coding Standards Adjustments

--- library/cleanup.php
+++ library/cleanup.php
@@ -171,12 +171,12 @@ if ( ! class_exists( 'Foundationpress_img_rebuilder' ) ) :
 	        }
 
 	        // Filter Through Class Segments & Find Alignment Classes and Size Classes
-            foreach ($class_segs as $class_seg) {
-                if ( substr( $class_seg, 0, 5 ) === "align" || substr( $class_seg, 0, 4 ) === "size" ) {
+            foreach ( $class_segs as $class_seg ) {
+                if ( substr( $class_seg, 0, 5 ) === 'align' || substr( $class_seg, 0, 4 ) === 'size' ) {
                     $new_classes[] = $class_seg;
                 }
             }
-            
+
             // Add Rebuilt Classes and Close The Tag
             $img .= ' class="' . implode($new_classes, ' ' ) . '" />';
 
