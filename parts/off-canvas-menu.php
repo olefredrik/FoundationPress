98d8defc849610b7127ef1bb536e09a6d1d85760
olefredrik <mail@olefredrik.com>
2015-12-02 23:42:24 +0100

    Remove direction filters for off-canvas

--- parts/off-canvas-menu.php
+++ parts/off-canvas-menu.php
@@ -10,5 +10,5 @@
 ?>
 
 <nav class="vertical menu off-canvas position-left" id="offCanvasleft" data-off-canvas data-position="left" data-accordion-menu role="navigation">
-  <?php foundationpress_mobile_off_canvas( apply_filters('filter_mobile_nav_position', 'mobile_nav_position') ); ?>
+  <?php foundationpress_mobile_off_canvas(); ?>
 </nav>
