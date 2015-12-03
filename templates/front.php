733a7b1b693d9f73e96f842b66a5cccf92be744b
olefredrik <mail@olefredrik.com>
2015-12-03 02:02:30 +0100

    Front page styling

--- templates/front.php
+++ templates/front.php
@@ -24,33 +24,31 @@ get_header(); ?>
 
 </header>
 
-	<div class="row">
-		<?php get_template_part( 'parts/check-if-sidebar-exist' ); ?>
-		<?php do_action( 'foundationpress_before_content' ); ?>
+<section id="front" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<header>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-				</header>
-				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-				<div class="entry-content">
-					<?php the_content(); ?>
-				</div>
-				<footer>
-					<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-					<p><?php the_tags(); ?></p>
-				</footer>
-				<?php do_action( 'foundationpress_page_before_comments' ); ?>
-				<?php comments_template(); ?>
-				<?php do_action( 'foundationpress_page_after_comments' ); ?>
-			</article>
-		<?php endwhile;?>
+	<div class="fp-intro">
 
-		<?php do_action( 'foundationpress_after_content' ); ?>
+	<?php do_action( 'foundationpress_before_content' ); ?>
 
-		</div>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+			<footer>
+				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php do_action( 'foundationpress_page_before_comments' ); ?>
+			<?php comments_template(); ?>
+			<?php do_action( 'foundationpress_page_after_comments' ); ?>
+		</article>
+	<?php endwhile;?>
 
-	<?php get_sidebar(); ?>
-</div>
+	<?php do_action( 'foundationpress_after_content' ); ?>
+
+	</div>
+</section>
+
 <?php get_footer(); ?>
