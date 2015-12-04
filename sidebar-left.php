39acfbdc73ee4a6cb44dc4c428697e020eb80960
olefredrik <mail@olefredrik.com>
2015-12-04 22:58:28 +0100

    Use semantic markup on template pages

--- sidebar-left.php
+++ sidebar-left.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<aside id="sidebar" class="small-12 large-4 columns large-pull-8">
+<aside class="sidebar">
 	<?php do_action( 'foundationpress_before_sidebar' ); ?>
 	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
 	<?php do_action( 'foundationpress_after_sidebar' ); ?>
