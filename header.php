9619409dfc8a591b72b5e55a327a37422f3ed377
olefredrik <mail@olefredrik.com>
2015-12-05 00:11:27 +0100

    Remove tab-bar flickering on page refresh

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="title-bar" data-responsive-toggle="site-navigation" data-hide-for="medium">
+		<div class="title-bar" data-responsive-toggle="site-navigation">
 			<button class="menu-icon" type="button" data-toggle="offCanvasleft"></button>
 			<div class="title-bar-title">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
@@ -57,5 +57,5 @@
 		</nav>
 	</header>
 
-	<section class="container" role="main">
+	<section class="container">
 		<?php do_action( 'foundationpress_after_header' ); ?>
