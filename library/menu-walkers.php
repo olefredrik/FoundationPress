9b30474a2bbae375019f5829906e8dad184ad8d3
olefredrik <mail@olefredrik.com>
2016-01-28 21:23:55 +0100

    Make second-level submenus fly out

--- library/menu-walkers.php
+++ library/menu-walkers.php
@@ -14,7 +14,7 @@
 
  	function start_lvl( &$output, $depth = 0, $args = array() ) {
  			$indent = str_repeat("\t", $depth);
-      $output .= "\n$indent<ul class=\"dropdown menu vertical\" data-dropdown-menu>\n";
+      $output .= "\n$indent<ul class=\"dropdown menu vertical\" data-toggle>\n";
  	}
  }
 
