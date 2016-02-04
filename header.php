f92487e672f2e8cc979b3f5663261dab6f630f2e
Adrian Bengtson <adrian@adagio.se>
2016-02-04 12:17:42 +0100

    Use bloginfo charset instead of hard coded utf-8
    (Taken from Underscores.)

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
 		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144.png">
