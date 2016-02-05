e32b83f6c1481942b3bd12299eaedb40f185d4a7
Adrian Bengtson <adrian@adagio.se>
2016-02-05 14:52:21 +0100

    Move template parts to template-parts folder
    Both content.php and content-none.php are template parts and therefore it is more logical to place them in template-parts folder.
    
    (Underscores does this too, but not default themes like TwentyTwelve.)

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ get_header(); ?>
 
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; // End have_posts() check. ?>
 
--- search.php
+++ search.php
@@ -18,11 +18,11 @@ get_header(); ?>
 	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 	<?php endif;?>
 
