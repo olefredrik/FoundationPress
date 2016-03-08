5fe8a33663079fd9e2b88ddafd6ea069244db8e0
Ole Fredrik Lie <mail@olefredrik.com>
2016-03-08 14:02:32 +0100

    Add child-theme support

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.3.0', 'all' );
+	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.3.0', 'all' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
