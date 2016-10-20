930587c2e183f11ba1b836894f1c336d7a51cf44
Eric Rihlmann <eric@rihlmann.com>
2016-10-20 16:19:17 -0400

    Optimized featured images
    Added new image sizes specifically for featured
    images that are optimized for landscape aspect
    ratio and updated SCSS for new ratio as well.

--- library/responsive-images.php
+++ library/responsive-images.php
@@ -7,6 +7,15 @@
  * @since FoundationPress 2.6.0
  */
 
+// Add featured image sizes
+//
+// Sizes are optimized and cropped for landscape aspect ratio
+// and optimized for HiDPI displays on 'small' and 'medium' screen sizes.
+add_image_size( 'featured-small', 640, 200, true ); // name, width, height, crop
+add_image_size( 'featured-medium', 1280, 400, true );
+add_image_size( 'featured-large', 1440, 400, true );
+add_image_size( 'featured-xlarge', 1920, 400, true );
+
 // Add additional image sizes
 add_image_size( 'fp-small', 640 );
 add_image_size( 'fp-medium', 1024 );
