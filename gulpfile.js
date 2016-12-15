50b2e3842c3cba7811188e6f8a945c4b6d63cad8
Ole Fredrik Lie <mail@olefredrik.com>
2016-12-15 23:05:39 +0100

    Remove my localhost URL from the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var cleanCSS    = require('gulp-clean-css');
 
 // Enter URL of your local server here
 // Example: 'http://localwebsite.dev'
-var URL = 'foundationpress:8888';
+var URL = '';
 
 // Check for --production flag
 var isProduction = !!(argv.production);
