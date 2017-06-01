65e2805922c95d5e030dc0814da9d3517f26abe8
Vincent Morel <studio12@Studio12-Vince.local>
2017-06-01 15:17:11 -0400

    Fixed Edit link
    Fixed small typo in Edit link in Page and Single that prevent
    traduction to work

--- page.php
+++ page.php
@@ -25,7 +25,7 @@
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php the_content(); ?>
-			<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+			<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
 		</div>
 		<footer>
 			<?php
