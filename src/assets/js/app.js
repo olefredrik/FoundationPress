436e09c22944deda7e57443fb19cae9a487445a7
Ole Fredrik Lie <mail@olefredrik.com>
2017-08-04 01:22:17 +0200

    Follow up on previous commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
