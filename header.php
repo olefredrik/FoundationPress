a46b04014f6163834718ca2dc94a12b9ed6a8a13
Ole Fredrik Lie <mail@olefredrik.com>
2017-08-04 16:10:44 +0200

    Remove the off-canvas-wrapper as this is no longer required

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	<div class="off-canvas-wrapper">
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
