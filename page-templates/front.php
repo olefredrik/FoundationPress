bf2a5fb705937624986a783a3fb5aa7cc3abc16d
Ole Fredrik Lie <mail@olefredrik.com>
2017-08-04 16:40:10 +0200

    Set path to dist folder for demo images in front template

--- page-templates/front.php
+++ page-templates/front.php
@@ -68,27 +68,27 @@ get_header(); ?>
 	</header>
 
 	<div class="semantic">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/semantic.svg" alt="semantic">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/semantic.svg" alt="semantic">
 		<h3>Semantic</h3>
 		<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
 	</div>
 
 	<div class="responsive">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/responsive.svg" alt="responsive">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/responsive.svg" alt="responsive">
 		<h3>Responsive</h3>
 		<p>You can build for small devices first. Then, as devices get larger and larger, layer in more complexity for a complete responsive design.</p>
 
 	</div>
 
 	<div class="customizable">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/customizable.svg" alt="customizable">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/customizable.svg" alt="customizable">
 		<h3>Customizable</h3>
 		<p>You can customize your build to include or remove certain elements, as well as define the size of columns, colors, font size and more.</p>
 
 	</div>
 
 	<div class="professional">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/professional.svg" alt="professional">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/professional.svg" alt="professional">
 		<h3>Professional</h3>
 		<p>Millions of designers and developers depend on Foundation. We have business support, training and consulting to help grow your product or service.</p>
 	</div>
