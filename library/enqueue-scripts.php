d5f4b7e068921d1e92872a9fb60d9e5e9a0b32af
Ole Fredrik Lie <mail@olefredrik.com>
2017-08-05 14:47:29 +0200

    Bump version number

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -13,17 +13,17 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), '2.10.0', 'all' );
+	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), '2.10.1', 'all' );
 
 	// Deregister the jquery version bundled with WordPress, as we're injecting the version we need with Webpack.
 	wp_deregister_script( 'jquery' );
 
 	// Enqueue Founation scripts
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), '2.10.0', true );
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), '2.10.1', true );
 
 	// Enqueue FontAwesome from CDN. Uncomment the line below if you don't need FontAwesome.
 	wp_enqueue_script( 'jquery', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
-	
+
 
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
