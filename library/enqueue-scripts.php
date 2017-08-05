807f53ec99c18f7ee8a7345609dc67a63e322fc9
Ole Fredrik Lie <mail@olefredrik.com>
2017-08-05 12:43:23 +0200

    Enqueue FontAwesome from CDN

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -21,6 +21,10 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// Enqueue Founation scripts
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array(), '2.10.0', true );
 
+	// Enqueue FontAwesome from CDN. Uncomment the line below if you don't need FontAwesome.
+	wp_enqueue_script( 'jquery', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
+	
+
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
