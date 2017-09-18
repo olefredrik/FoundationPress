405cf226459048e300dfad6cba33905610ad4870
JP Mac <josh@whosjosh.com>
2017-09-18 10:02:59 -0700

    Widget area element change. Small spelling correction.

--- library/widget-areas.php
+++ library/widget-areas.php
@@ -12,8 +12,8 @@ function foundationpress_sidebar_widgets() {
 		'id' => 'sidebar-widgets',
 		'name' => __( 'Sidebar widgets', 'foundationpress' ),
 		'description' => __( 'Drag widgets to this sidebar container.', 'foundationpress' ),
-		'before_widget' => '<article id="%1$s" class="widget %2$s">',
-		'after_widget' => '</article>',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget' => '</section>',
 		'before_title' => '<h6>',
 		'after_title' => '</h6>',
 	));
@@ -22,8 +22,8 @@ function foundationpress_sidebar_widgets() {
 		'id' => 'footer-widgets',
 		'name' => __( 'Footer widgets', 'foundationpress' ),
 		'description' => __( 'Drag widgets to this footer container', 'foundationpress' ),
-		'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
-		'after_widget' => '</article>',
+		'before_widget' => '<section id="%1$s" class="large-4 columns widget %2$s">',
+		'after_widget' => '</section>',
 		'before_title' => '<h6>',
 		'after_title' => '</h6>',
 	));
