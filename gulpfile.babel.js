75b4a7d67cb48a75c912a9ee740a243880303d2f
Ednilson Maia <dina@veda.com.br>
2017-11-20 19:52:33 -0200

    Correct images path in gulpfile.babel.js (Missed in #1155)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -210,5 +210,5 @@ function watch() {
   gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
   gulp.watch('**/*.php').on('all', browser.reload);
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
-  gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
+  gulp.watch('src/assets/images/**/*').on('all', gulp.series(images, browser.reload));
 }
