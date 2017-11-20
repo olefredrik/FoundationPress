3dfbe9508f4eb7d5b52a52174e450f3b35eb390a
Ednilson Maia <dina@veda.com.br>
2017-11-20 20:08:57 -0200

    Remove an unnecessary echo. The "the_post_thumbnail_url" function already returned an echo.

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -2,6 +2,6 @@
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+	<header class="featured-hero" role="banner" data-interchange="[<?php the_post_thumbnail_url('featured-small'); ?>, small], [<?php the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php the_post_thumbnail_url('featured-large'); ?>, large], [<?php the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
 	</header>
 <?php endif;
