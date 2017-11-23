2639a86a86853daf809913918c7814afdde6e812
Alda Vigdis Skarphedinsdottir <aldavigdis@aldavigdis.is>
2017-11-23 21:10:28 +0100

    Adding aria-label attribute to mobile menu hamburger button

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<header class="site-header" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
 			<div class="title-bar-left">
-				<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 				<span class="site-mobile-title title-bar-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</span>
