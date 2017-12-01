41688ea5a02370186d30c6ca250e08b518ad2775
Giolliano Sulit <giolliano@blissmedia.com.au>
2017-12-01 15:47:51 +1100

    Cleanup page.php indentation

--- page.php
+++ page.php
@@ -10,18 +10,18 @@
  * @since FoundationPress 1.0.0
  */
 
- get_header(); ?>
+get_header(); ?>
 
- <?php get_template_part( 'template-parts/featured-image' ); ?>
- <div class="main-container">
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+<div class="main-container">
     <div class="main-grid">
-     <main class="main-content">
-    	 <?php while ( have_posts() ) : the_post(); ?>
-    	 	<?php get_template_part( 'template-parts/content', 'page' ); ?>
-    		<?php comments_template(); ?>
-    	 <?php endwhile;?>
-     </main>
-    <?php get_sidebar(); ?>
+        <main class="main-content"[>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+                <?php comments_template(); ?>
+            <?php endwhile;?>
+        </main>
+        <?php get_sidebar(); ?>
     </div>
- </div>
- <?php get_footer();
+</div>
+<?php get_footer();
