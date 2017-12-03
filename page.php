3e6fb68bcb8044b16fd2705ee41bae5142d7dce8
Giolliano Sulit <giolliano@blissmedia.com.au>
2017-12-04 10:09:50 +1100

    Remove stray ‘[‘ and update single.php sidebar indentation

--- page.php
+++ page.php
@@ -15,7 +15,7 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
     <div class="main-grid">
-        <main class="main-content"[>
+        <main class="main-content">
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'template-parts/content', 'page' ); ?>
                 <?php comments_template(); ?>
