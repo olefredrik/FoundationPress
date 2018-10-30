70af4ce85c1a759e10a9216568af623163cd5668
Julian Weiland <jw@werbeagenten.de>
2018-10-30 11:49:12 +0100

    Add separate editor stylesheet

--- library/theme-support.php
+++ library/theme-support.php
@@ -42,8 +42,12 @@ if ( ! function_exists( 'foundationpress_theme_support' ) ) :
 		add_theme_support( 'wc-product-gallery-lightbox' );
 		add_theme_support( 'wc-product-gallery-slider' );
 
+		add_theme_support( 'editor-styles' );
+
+		add_editor_style( get_stylesheet_directory_uri() . '/dist/assets/css/editor.css' );
+
 		// Add foundation.css as editor style https://codex.wordpress.org/Editor_Style
-		add_editor_style( 'dist/assets/css/' . foundationpress_asset_path( 'app.css' ) );
+		// add_editor_style( 'dist/assets/css/' . foundationpress_asset_path( 'editor.css' ) );
 	}
 
 	add_action( 'after_setup_theme', 'foundationpress_theme_support' );
